f173bccca648b6e2fa6805911f801dd33ac20c5d
Roman.Gaikov <gaikov@inbox.ru>
2025-05-28 19:26:17 +0300

    network ServerSocket

--- ServerSources/main.cpp
+++ ServerSources/main.cpp
@@ -1,6 +1,7 @@
 #include "Core/debug/LogStdOut.h"
 #include "Engine/Platform.h"
 #include "Networking/Net.h"
+#include "Networking/SocketServer.h"
 #include "nsLib/log.h"
 
 Platform* App_GetPlatform() {
@@ -15,6 +16,13 @@ int main(int argc, char *argv[]) {
     Log::Info("Hello World!");
     if (nsNet::Init()) {
         Log::Info("Server initialized");
+    }
+
+    nsServerSocket server;
+    if (server.Listen(3333)) {
+        while (true) {
+            auto client = server.Accept();
+        }
     }
 
     nsNet::Release();
