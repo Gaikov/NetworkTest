6614fe5538659d47257d3a706fd8b47e3946d94e
Roman.Gaikov <gaikov@inbox.ru>
2025-05-28 19:02:19 +0300

    network initialization

--- ServerSources/main.cpp
+++ ServerSources/main.cpp
@@ -1,5 +1,6 @@
 #include "Core/debug/LogStdOut.h"
 #include "Engine/Platform.h"
+#include "Networking/Net.h"
 #include "nsLib/log.h"
 
 Platform* App_GetPlatform() {
@@ -12,7 +13,10 @@ int main(int argc, char *argv[]) {
     Log::Init();
     Log::Shared()->AddPolicy(&out);
     Log::Info("Hello World!");
+    if (nsNet::Init()) {
+        Log::Info("Server initialized");
+    }
 
-
+    nsNet::Release();
     Log::Release();
 }
